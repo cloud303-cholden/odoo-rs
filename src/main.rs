3ff369e255daccef3676a37d2178c4ba92f9752a
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-05-31 16:32:43 -0600

    Experiment with different data serializers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use xmlrpc::Error;
+use xmlrpc::{Error};
+use serde_xmlrpc::Value;
+use serde::Serialize;
 
-use odoorpc::Proxy;
+use odoorpc::{Client, Data};
+
+#[derive(Serialize)]
+struct ResPartner<'a> {
+    name: &'a str,
+    email: &'a str,
+}
 
 fn main() -> Result<(), Error> {
-    let mut odoo = Proxy::new(
+    let mut odoo = Client::new(
         "dev",
         "admin",
         "admin",
@@ -17,6 +25,24 @@ fn main() -> Result<(), Error> {
         .browse(1)
         .get("c_aws_account_name")?;
     println!("{partners:?}");
+
+    let partner = odoo
+        .env("res.partner")
+        // .create(Data::from([
+        //     ("name".to_string(), Value::from("Rust 1")),
+        //     ("email".to_string(), Value::from("rust1@example.com")),
+        // ]).0)?;
+        .create2(serde_xmlrpc::to_value(ResPartner {
+            name: "Rust 1",
+            email: "rust1@example.com",
+        }).unwrap())?;
+    println!("{partner}");
+    let name = partner.get("name")?;
+    println!("{name:?}");
+    // let res = partner.write(Data::from([
+    //     ("name".to_string(), Value::from("Rust 6")),
+    // ]).0)?;
+    // println!("{res}");
 
     Ok(())
 }
