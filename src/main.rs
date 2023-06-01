fd7fed6042e247b2eecf59d1317cb076ecde2641
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-06-01 13:12:26 -0600

    Fix generics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use serde_xmlrpc::Value;
 use serde::Serialize;
-use reqwest::Error;
 
-use odoorpc::{Client, Data};
+use odoorpc::{Client, RequestError};
 
 #[derive(Serialize, Debug)]
 struct ResPartner<'a> {
@@ -10,34 +8,35 @@ struct ResPartner<'a> {
     email: &'a str,
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), RequestError> {
     let mut odoo = Client::new(
         "dev",
         "admin",
         "admin",
         "http://127.0.0.1:8069",
+        "res.users".to_string(),
     )?;
     println!("{}", odoo.browse(2));
-    println!("{:?}", odoo.browse(2).get("name")?);
-
-    let partners = odoo
-        .env("c.aws.accounts")
-        .browse(1)
-        .get("c_aws_account_name")?;
-    println!("{partners:?}");
-
-    let v = ResPartner {
-        name: "Rust 1",
-        email: "rust1@example.com",
-    };
-    println!("{v:?}");
-
-    let partner = odoo
-        .env("res.partner")
-        .create(v)?;
-    println!("{partner}");
-    let name = partner.get("name")?;
-    println!("{name:?}");
+    // println!("{:?}", odoo.browse(2).get("name")?);
+    //
+    // let partners = odoo
+    //     .env("c.aws.accounts")
+    //     .browse(1)
+    //     .get("c_aws_account_name")?;
+    // println!("{partners:?}");
+    //
+    // let v = ResPartner {
+    //     name: "Rust 1",
+    //     email: "rust1@example.com",
+    // };
+    // println!("{v:?}");
+    //
+    // let partner = odoo
+    //     .env("res.partner")
+    //     .create(v)?;
+    // println!("{partner}");
+    // let name = partner.get("name")?;
+    // println!("{name:?}");
     // let res = partner.write(Data::from([
     //     ("name".to_string(), Value::from("Rust 6")),
     // ]).0)?;
