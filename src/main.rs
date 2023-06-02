95fc6fda65c1c601e46b73dec5c8a568b1707f3c
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-06-02 16:59:40 -0600

    Add create, write, search, read, and unlink

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-// use serde::Serialize;
+use serde::Serialize;
+use serde_json::json;
 
 use odoorpc::Client;
 
-// #[derive(Serialize, Debug)]
-// struct ResPartner<'a> {
-//     name: &'a str,
-//     email: &'a str,
-// }
+#[derive(Serialize, Debug)]
+struct ResPartner<'a> {
+    name: &'a str,
+    email: &'a str,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,28 +21,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", odoo.browse(2));
     println!("{}", odoo.browse(2).get("name").await?);
 
-    // let partners = odoo
-    //     .env("c.aws.accounts")
-    //     .browse(1)
-    //     .get("c_aws_account_name")?;
-    // println!("{partners:?}");
-    //
-    // let v = ResPartner {
-    //     name: "Rust 1",
-    //     email: "rust1@example.com",
-    // };
-    // println!("{v:?}");
-    //
-    // let partner = odoo
-    //     .env("res.partner")
-    //     .create(v)?;
-    // println!("{partner}");
-    // let name = partner.get("name")?;
-    // println!("{name:?}");
-    // let res = partner.write(Data::from([
-    //     ("name".to_string(), Value::from("Rust 6")),
-    // ]).0)?;
-    // println!("{res}");
+    let account = odoo
+        .env("c.aws.accounts".to_string())
+        .browse(1)
+        .get("c_aws_account_name")
+        .await?;
+    println!("{account:?}");
 
+    let v = ResPartner {
+        name: "Rust 2",
+        email: "rust2@example.com",
+    };
+    println!("{v:?}");
+    let partner = odoo
+        .env("res.partner".to_string())
+        .create(serde_json::to_value(v)?)
+        .await?;
+    println!("{partner}");
+
+    let name = partner.get("name").await?;
+    println!("{name:?}");
+
+    partner.write(json!({
+        "name": "Rust 6",
+    })).await?;
+
+    odoo.search(json!([
+        ["name", '=', "Rust 6"],
+    ])).await?;
+    println!("{}", odoo);
+
+    odoo.unlink().await?;
+    odoo.unlink().await?;
+
+    odoo.read(json!([
+        "name", "id", "email",
+    ])).await?;
+
+    odoo.search_read(
+        json!([
+            ["name", "=", "Rust 2"],
+        ]),
+        json!([
+            "name", "company_id", "email",
+        ]),
+    ).await?;
     Ok(())
 }
